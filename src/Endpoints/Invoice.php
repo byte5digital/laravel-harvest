56e92b7ef54c939a50c95b6111fa1ac63ffcc0b3
Naoray Kroaths <krishan.koenig@googlemail.com>
2018-04-17 15:33:22 +0200

    add additional invoice params

--- src/Endpoints/Invoice.php
+++ src/Endpoints/Invoice.php
@@ -2,6 +2,8 @@
 
 namespace Byte5\LaravelHarvest\Endpoints;
 
+use Carbon\Carbon;
+
 class Invoice extends BaseEndpoint
 {
     /**
@@ -19,4 +21,64 @@ class Invoice extends BaseEndpoint
     {
         return \Byte5\LaravelHarvest\Models\Invoice::class;
     }
+
+	/**
+	 * @param $id
+	 */
+    public function client($id)
+    {
+        $this->params += ['client_id' => $id];
+    }
+
+	/**
+	 * @param $id
+	 */
+    public function project($id)
+    {
+        $this->params += ['project_id' => $id];
+    }
+    
+	/**
+	 * @param $state
+	 */
+    public function state($state)
+    {
+        $this->params += ['state' => $state];
+    }
+
+	/**
+	 * @param $dateTime
+	 */
+    public function updatedSince($dateTime)
+    {
+    	if (! $dateTime instanceof Carbon) {
+    		$dateTime = Carbon::parse($dateTime);
+	    }
+
+        $this->params += ['updated_since' => $dateTime->toIso8601ZuluString()];
+    }
+
+	/**
+	 * @param $date
+	 */
+    public function from($date)
+    {
+	    if (! $date instanceof Carbon) {
+		    $date = Carbon::parse($date);
+	    }
+
+        $this->params += ['from' => $date->format('Y-m-d')];
+    }
+
+	/**
+	 * @param $date
+	 */
+	public function to($date)
+	{
+		if (! $date instanceof Carbon) {
+			$date = Carbon::parse($date);
+		}
+
+		$this->params += ['to' => $date->format('Y-m-d')];
+	}
 }
